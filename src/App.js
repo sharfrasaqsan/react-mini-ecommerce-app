bf771b3488a52ab90eab7079a6a72d8f3a9b4750
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2025-07-11 11:20:34 +0530

    components & routes updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
